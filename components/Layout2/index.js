0a1fefb9c19ca1c02501ea3840413ae76c141d61
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-10 07:39:25 +0330

    Update index.js

--- components/Layout2/index.js
+++ components/Layout2/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import IMG from 'next/image';
 import { Layout, Menu, Breadcrumb } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import st2 from "./layout2.module.css"
+import Link from 'next/link';
 const { Header, Content, Footer } = Layout;
+
+function getItem(label, key, icon, children, type) {
+  return { key,icon,children,label,type, };
+}
+
+const items = [
+  getItem(<Link href='/'>خانه</Link>, '1'),
+  
+  getItem(<Link href='/nazarSanji'>نظرسنجی</Link>, '2')
+
+];
 
 export default ({ children }) => (
   <Layout className="layoutyyy">
@@ -11,19 +22,20 @@ export default ({ children }) => (
       <div className={st2.logo}>
           <IMG src={"/img/meratLogo.png"} width='170px' height='71px' ></IMG>
       </div>
-      <Menu className={st2.mymenu} 
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        items={[
-            {
-              label: ' فروشگاه ', icon:<MailOutlined /> ,
-              children: [{ label: 'فروشگاه مرآت شاپ' },{ label: 'سبد خرید' },{ label: 'پرداخت' }],
-            },
-            { label: 'نظرسنجی', disabled: true, },
-            { label: 'گفتگو' },
-          ]}
-      />
+      <Menu className={st2.mymenu} mode="horizontal">
+        <Menu.Item key={1}><Link href='/'>خانه</Link></Menu.Item>
+        <Menu.SubMenu key={2} title="فروشگاه">
+          <Menu.Item key={3}>سبد خرید</Menu.Item>
+          <Menu.Item key={4}>پرداخت</Menu.Item>
+        </Menu.SubMenu>
+        <Menu.Item key={5}>
+          <Link href='/nazarSanji'>نظرسنجی</Link>
+        </Menu.Item>
+        <Menu.Item key={6}>چت روم</Menu.Item>
+      </Menu>
+
+      <Menu mode='horizontal' theme='light' items={items} />
+      
     </Header>
     <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
