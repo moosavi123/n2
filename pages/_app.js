ce7118f31233bd91d0292973270740c4521cef2a
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-10 07:39:57 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';  // یا
+// import 'antd/dist/antd.dark.css';  // یا
+// import 'antd/dist/antd.compact.css';  // یا
 import 'swiper/css';
 import Layout2 from '../components/Layout2';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout2>
-      <Component {...pageProps} />
-    </Layout2>
+    
+      <Layout2>
+        <Component {...pageProps} />
+      </Layout2>
+    
+    
   );
 }
 
