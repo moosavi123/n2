7626086700fa063b1ab84b52930c908de137fbf9
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-15 10:06:52 +0330

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 const pricing = ( {ppp} ) => {
     return ( 
-        <>
-            <h2>1ddfddfg</h2>
-            <h2>2ddfddfg</h2>
-            <h2>3ddfddfg</h2>
-            <h1> { ppp.length } </h1>
-        </>
+      <>
+          {
+          ppp.map((p, i)=>{
+              return (
+                  <img key={i} src={p.source_url} alt={p.slug} width='600' />
+                  // <IMG key={i} src={p.source_url} alt={p.slug} layout='fill' />
+              )
+          })
+          }
+      </>
      );
 }
  
 export default pricing;
 
 export const getStaticProps = async () => {
-    const res = await fetch('http://meratkish.ir/wp-json/wp/v2/posts');
+    const res = await fetch('http://meratyar.com/wp-json/wp/v2/media');
     // const res = await ax('https://jsonplaceholder.typicode.com/users');
     const ppp = await res.json();
     return {
       props: { ppp }
     }
-  }
+}
