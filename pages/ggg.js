6b0f099cda12e5607f6918b7996838fe12694eea
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-15 10:07:19 +0330

    Update ggg.js

--- pages/ggg.js
+++ pages/ggg.js
@@ -1,24 +1,24 @@
+import IMG from 'next/image';
 
 const ggg = ( {ppp} ) => {
     return ( 
-        <div>
-            <h1>1kjhjkhhfsf</h1>
-            <h1>2kjhjkhhfsf</h1>
-            <h1>3kjhjkhhfsf</h1>
-            <h1>kjhjkhhfsf</h1>
-            <h1>kjhjkhhfsf</h1>
-            <h1>kjhjkhhfsf</h1>
+        <>
             {
-                ppp.map((p, i)=>{return ( <i key={i}> {p.id} <br /> </i> )})
+            ppp.map((p, i)=>{
+                return (
+                    <img key={i} src={p.source_url} alt={p.slug} />
+                    // <IMG key={i} src={p.source_url} alt={p.slug} layout='fill' />
+                )
+            })
             }
-        </div>
+        </>
      );
 }
  
 export default ggg;
 
 export const getServerSideProps = async () => {
-    const res = await fetch('http://meratkish.ir/wp-json/wp/v2/posts');
+    const res = await fetch('http://meratkish.ir/wp-json/wp/v2/media');
     // const res = await ax('https://jsonplaceholder.typicode.com/users');
     const ppp = await res.json();
     return {
