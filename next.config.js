31244fc8fd85d69b203e13bc33715902f541c8ce
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-17 11:45:44 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: ['meratkish.ir', 'irsbf.ir', 'www.meratyar.com'],
+  },
+
 }
 
 module.exports = nextConfig
