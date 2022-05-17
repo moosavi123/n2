86ccfed169345c9740f1d496e6fef15db525a1b0
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-17 11:45:35 +0330

    Update ggg.js

--- pages/ggg.js
+++ pages/ggg.js
@@ -1,23 +1,25 @@
+import { Carousel } from 'antd';
 import IMG from 'next/image';
 
 const ggg = ( {ppp} ) => {
     return ( 
         <>
+            <Carousel autoplay>
             {
-            ppp.map((p, i)=>{
-                return (
-                    <img key={i} src={p.source_url} alt={p.slug} />
-                    // <IMG key={i} src={p.source_url} alt={p.slug} layout='fill' />
-                )
-            })
+              ppp.map((p, i) => (
+                <div key={i}>
+                    <IMG src={p.source_url} alt={p.slug} height={500} width={1100} />
+                </div>
+            ))
             }
+            </Carousel>
         </>
      );
 }
  
 export default ggg;
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
     const res = await fetch('http://meratkish.ir/wp-json/wp/v2/media');
     // const res = await ax('https://jsonplaceholder.typicode.com/users');
     const ppp = await res.json();
