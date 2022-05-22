d2154372662ab8e713c34ad322a0b0fe3eedcdc9
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-22 15:24:17 +0330

    Create shop.js

---
+++ pages/shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const shop = () => {
+    return ( 
+        <div>
+            <h1>فروشگاه مرات شاپ</h1>
+            <h2>در دست ساخت</h2>
+        </div>
+     );
+}
+ 
+export default shop;
