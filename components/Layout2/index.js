350db430835bda414c778955196bd0d0ca59550b
سیدحسین موسوی <70389221+moosavi123@users.noreply.github.com>
2022-05-22 15:24:34 +0330

    Update index.js

--- components/Layout2/index.js
+++ components/Layout2/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import IMG from 'next/image';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Row, Col, Divider } from 'antd';
 import st2 from "./layout2.module.css"
 import Link from 'next/link';
+import { WhatsAppOutlined } from '@ant-design/icons';
+
 const { Header, Content, Footer } = Layout;
 
 function getItem(label, key, icon, children, type) {
@@ -11,12 +13,12 @@ function getItem(label, key, icon, children, type) {
 
 const items = [
   getItem(<Link href='/'>خانه</Link>, '1'),
-  getItem('فروشگاه ▼', '2', null, [getItem(<Link href='/test'>سبد خرید</Link>, '21'), getItem('پرداخت', '22')]),
+  getItem('فروشگاه ▼', '2', null, [getItem(<Link href='/shop'>فروشگاه مرات شاپ</Link>, '21'), getItem(<Link href='/cart'>سبد خرید</Link>, '22'), getItem('پرداخت', '23')]),
   getItem(<Link href='/nazarSanji'>نظرسنجی</Link>, '3'),
   getItem(<Link href='/ggg'>چت روم</Link>, '4'),
   getItem('پزشکی ▼', '5', null, [getItem('معاینات و چک آپ منظم پزشکی', '51'), getItem('ارتباط با درمانگاها', '52')]),
   getItem('آموزش ▼', '6', null, [getItem('مشاوره کسب و کار', '61'), getItem('مشاوره کنکور', '62')]),
-  getItem('آکادمی فولاد ▼', '7', null, [getItem('سبد خرید', '71'), getItem('پرداخت', '72')]),
+  getItem('آکادمی فولاد ▼', '7', null, []),
   getItem(<Link href='/pricing'>باشگاه مشتریان</Link>, '8'),
   getItem('رزرو هتل ▼', '9', null, [getItem('هتل مرات مشهد', '91'), getItem('هتل ملیسا مشهد', '92')]),
   getItem(<Link href='/foolad'>پنل کاربری</Link>, '10'),
@@ -29,18 +31,43 @@ const Layout2 = ({ children }) => (
           <IMG src={"/img/meratLogo.png"} width='170px' height='71px' alt='merat' ></IMG>
       </div>
 
-      <Menu className={st2.mymenu} mode='horizontal' theme='dark' items={items} />
+      <Menu className={st2.mymenu} mode='horizontal' theme='dark' items={items} defaultSelectedKeys={['1']} />
       
     </Header>
     <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '6px 0', direction: 'rtl' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
+        <Breadcrumb.Item>صفحه نخست</Breadcrumb.Item>
+        <Breadcrumb.Item>آموزش</Breadcrumb.Item>
+        <Breadcrumb.Item>مشاوره کسب و کار</Breadcrumb.Item>
       </Breadcrumb>
       <div className={st2.site_layout_content}>{ children }</div>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <Footer style={{direction:'rtl',backgroundColor:'gray' }}>
+      <Divider style={{border:'2px solid blue'}} />
+      <Row>
+        <Col span={6}>
+          <p className={st2.ffont}>تماس با ما</p>
+          <p>آدرس ایمیل : info@meratkish.ir</p>
+          <p>شماره تلفن : ۶ -۸۸۷۵۴۹۴۴ (۰۲۱)</p>
+          <p>نشانی : تهران - خیابان شهید احمد قصیر (بخارست) - خیابان هشتم - شماره ۱۵ - طبقه سوم</p>
+        </Col>
+        <Col span={6}>
+          bbb
+        </Col>
+        <Col span={6}>
+          ccc
+        </Col>
+        <Col span={6}>
+          <p className={st2.ffont}>شبکه های اجتماعی</p>
+          <p>
+            <WhatsAppOutlined style={{fontSize:'44px'}} />  واتس اپ
+          </p>
+          
+        </Col>
+      </Row>
+      <Divider orientation='center' style={{fontSize:'small'}} >تمامی حقوق سایت برای ( مرآت کیش ) محفوظ است</Divider>
+    </Footer>
+    
   </Layout>
 );
 
